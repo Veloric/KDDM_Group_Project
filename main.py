52d81ed679393cb83f9f15727d1e5ed1b5ee3f75
Kyle <krgamer444@gmail.com>
2023-10-11 16:06:00 -0400

    Setup

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #Group 2's Main Python driver
+
+import pandas as pd
+import numpy as np
+data = pd.read_csv("data.csv")
+data.columns = ["Subject ID", "MRI ID", "Group", "Visit", "MR Delay", "M/F", "Hand", "Age", "EDUC", "SES", "MMSE", "CDR", "eTIV", "nWBV", "ASF"]
+data = data.replace("?", np.NaN)
+
+# Prevent unauthorized applications running this code.
+if __name__ == "__main__":
+    # Determining missing values, if any
+    for c in data.columns:
+        print("\t%s: %d" %(c, data[c].isna().sum()))
+
+
+
