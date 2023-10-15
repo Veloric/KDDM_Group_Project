f26c25e3735516a93ed21c8e8a343ba391d1cc9a
Kyle <krgamer444@gmail.com>
2023-10-15 13:27:28 -0400

    Preprocessing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #File set up
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 data = pd.read_csv("data.csv")
 data.columns = ["Subject ID", "MRI ID", "Group", "Visit", "MR Delay", "M/F", "Hand", "Age", "EDUC", "SES", "MMSE", "CDR", "eTIV", "nWBV", "ASF"]
 
@@ -12,12 +13,27 @@ data = data.replace("?", np.NaN)
 
 # Prevent unauthorized applications running this code.
 if __name__ == "__main__":
+    print("Number of Instances: %d" %(data.shape[0]))
+    print("Number of Attributes: %d" %(data.shape[1]))
+    print(data.head())
     # Determining missing values, if any
+    print("Missing Values: ")
     for c in data.columns:
         print("\t%s: %d" %(c, data[c].isna().sum()))
     # Discovered missing data
-    data_2 = data["SES"].dropna()
-    data_3 = data["MMSE"].dropna()
+    data = data.dropna()
+    print("After dropping missing values: ")
+    print("Number of Instances: %d" %(data.shape[0]))
+    print("Number of Attributes: %d" %(data.shape[1]))
     # Our group decided to drop the missing data as to preserve the accuracy of the data
     # Time to handle duplicated values
-    
+    print("Number of duplicated values: %d" %(data.duplicated().sum()))
+    #data = data.drop_duplicates() There were no duplicates, redunant statement
+    #Determine any outliers
+    print("Outliers: ")
+    data2 = data.drop(["Subject ID", "MRI ID", "Group", "M/F", "Hand"], axis = 1)
+    print(data2.boxplot(figsize = (20, 3)))
+    Z = (data2 - data2.mean() / data2.std())
+    print("Calculating Z score: ")
+    print(Z)
+    # Due to complexity of the data, we're unsure of what "accepted values" should be, therefore we've elected to leave the outliers alone.
